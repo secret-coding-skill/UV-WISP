86b6b2b281613cddbe38c974c0de915e75523217
secret-coding-skill <asingnavong69420@gmail.com>
2025-06-22 12:17:18 -0700

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -2,7 +2,7 @@ export default {
   prefix: '/uv/',
   bare: 'wss://wisp.pro', 
   encodeUrl: true,
-  handler: '/uv/uv.handler.js',
-  client: '/uv/uv.client.js',
-  sw: '/uv/uv.sw.js',
+  handler: 'uv.handler.js',
+  client: 'uv.client.js',
+  sw: 'uv.sw.js',
 };
